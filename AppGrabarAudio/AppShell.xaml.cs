cb19c29c691058e20b4c0c4fcf95e96a8505d8ca
EdgarGmz <edgar_gomez90@outlook.com>
2026-06-16 21:03:13 -0600

    Se agrega AppGrabarAudio

---
+++ AppGrabarAudio/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppGrabarAudio;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppGrabarAudio/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Plugin.Maui.Audio;
+namespace AppGrabarAudio;
+
+public partial class MainPage : ContentPage
+{
+	readonly IAudioManager _audioManager;
+	readonly IAudioRecorder _audioRecorder;
+
+	public MainPage(IAudioManager audioManager)
+	{
+		InitializeComponent();
+		_audioManager = audioManager;
+		_audioRecorder = audioManager.CreateRecorder();
+	}
+
+	private async void BtnIniciarGrabacion_Clicked(object sender, EventArgs e)
+	{
+		// Solicitar permiso de micrófono
+		if(await Permissions.RequestAsync<Permissions.Microphone>() != PermissionStatus.Granted)
+		{
+			await DisplayAlertAsync("¡Permiso denegado!", "Permiso de micrófono no habilitado.", "OK");
+			return;
+		}
+
+		// Iniciar grabación de audio
+		if(!_audioRecorder.IsRecording)
+		{
+			await _audioRecorder.StartAsync();
+			BtnIniciarGrabacion.Text = "Detener Grabación";
+			return;
+		}
+		else
+		{
+			// Detener grabación de audio
+			BtnIniciarGrabacion.Text = "Procesando...";
+			BtnIniciarGrabacion.IsEnabled = false;
+			
+
+			// Detener la grabación y obtener el audio grabado
+			var recorderAudio = await _audioRecorder.StopAsync();
+			
+			BtnIniciarGrabacion.IsEnabled = true;
+			var player = AudioManager.Current.CreatePlayer(recorderAudio.GetAudioStream());
+
+			// Reproducir el audio grabado
+			player.Play();
+			BtnIniciarGrabacion.Text = "Iniciar Grabación";
+			BtnIniciarGrabacion.IsEnabled = false;
+		}
+	}
+}
